85e3f2b49242a8560505a79a143cc6cec8c72f20
Kolowitz <bkolowitz@gmail.com>
2016-11-10 13:50:53 -0500

    blah

--- src/main/java/JsonUtil.java
+++ src/main/java/JsonUtil.java
@@ -7,6 +7,8 @@ public class JsonUtil {
   public static String toJson(Object object) {
     return new Gson().toJson(object);
   }
+
+  
  
   public static ResponseTransformer json() {
     return JsonUtil::toJson;
